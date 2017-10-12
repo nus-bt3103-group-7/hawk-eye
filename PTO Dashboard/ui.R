8d57a7ec4d79b662e7bd28752e361f024e56c13c
D F <Daryl@Daryls-MacBook-Pro-2.local>
2017-10-12 18:58:16 +0800

    added tabs and conditional sidebars for splitting the PTO and commuter view.

--- PTO Dashboard/ui.R
+++ PTO Dashboard/ui.R
@@ -4,36 +4,43 @@ library(ggplot2)
 library(leaflet)
 library(plotly)
 
-
-shinyUI(
-  pageWithSidebar(
-    
-    
-    headerPanel(title="Service Disruption at Bishan Station"),
-
-    sidebarPanel(width=2,
-                 selectInput("radius", "Radius:",
-                             c("400m" = 400,
-                               "800m" = 800,
-                               "1000m" = 1000,
-                               "1500m" = 1500,
-                               "2000m" = 2000),
-                             selected = 800)
+shinyUI(pageWithSidebar(
+  
+  headerPanel(title="Service Disruption at Bishan Station"),
+  
+  sidebarPanel(
+    conditionalPanel(condition="input.conditionedPanels==1",
+                     width=1,
+                     selectInput("radius", "Radius:",
+                                 c("400m" = 400,
+                                   "800m" = 800,
+                                   "1000m" = 1000,
+                                   "1500m" = 1500,
+                                   "2000m" = 2000),
+                                 selected = 800)
     ),
-
-    mainPanel(
+    conditionalPanel(condition="input.conditionedPanels==2",
+                     #yikun and yixin - add sidebar panel codes here
+                     helpText("Content Panel 2")
+    ) 
+  ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("PTO View", value=1,
+               fluidRow(
+                 leafletOutput("groundMap")
+               ), 
+               fluidRow("Matching Commuters to Destinations", align="center"), 
+               fluidRow(plotlyOutput(outputId = "plotlyBarChart")
+               ),
+               fluidRow(plotlyOutput(outputId = "plotlySurfaceChart")
+               )
+               ), 
       
-                 fluidRow(
-                   leafletOutput("groundMap")
-                 ), 
-                 fluidRow("Matching Commuters to Destinations", align="center"), 
-                 fluidRow(plotlyOutput(outputId = "plotlyBarChart")
-                   ),
-                 fluidRow(plotlyOutput(outputId = "plotlySurfaceChart")
-                 )
-                 
-    )  
-
+      tabPanel("Comuter View ", 
+               #yikun and yixin - add ui main panel codes here,
+               value=2)
+      , id = "conditionedPanels"
+    )
   )
-)
-
+))
