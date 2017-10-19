aec60c302495c57c236ca6ba83b65d5a8244f828
D F <Daryl@Daryls-MacBook-Pro-2.local>
2017-10-20 00:26:46 +0800

    Major change to flow of server.R to make graphs load quicker. no change to overall algo

--- src/server.R
+++ src/server.R
@@ -20,12 +20,19 @@ pullFromFirebase <- function(url){
   as.data.frame(dataList)
 }
 ###############
-
 #pulling data from firebase db
 
 commuterData <- pullFromFirebase("https://bt3101-07.firebaseio.com/user_data.json?auth=MULTPLyGcPig4Hd2aCplVibPdIm3bpHoiT1LJG3R")
 allStoppingPoints <- pullFromFirebase("https://bt3101-07.firebaseio.com/bus_stop.json?auth=MULTPLyGcPig4Hd2aCplVibPdIm3bpHoiT1LJG3R")
 colnames(allStoppingPoints)[3] <- "longtitude"
+
+#################################################################################
+#static parameters
+
+#station name
+stationName <- "Bishan MRT"
+#kent ridge coordinates
+stationLatLong <-c(103.8469711, 1.3513141)
 
 #################################################################################
 #get column ids
@@ -37,115 +44,36 @@ station_long_col <- which(colnames(commuterData) == "station_long")
 station_lat_col <- which(colnames(commuterData) == "station_lat")
 stop_lat_col <- which(colnames(allStoppingPoints) == "latitude")
 stop_long_col <- which(colnames(allStoppingPoints) == "longtitude")
+stop_name_col <- which(colnames(allStoppingPoints) == "name")
 
 #################################################################################
 
 shinyServer(function(input, output) {
   
-  output$groundMap <- renderLeaflet({
-
-  #################################################################################
-  #input parameters
-  
-  #station name
-  stationName <- "Bishan MRT"
-  
-  #kent ridge coordinates
-  stationLatLong <- c(103.7827029, 1.2924922)
-  stationLatLong <-c(103.8469711, 1.3513141)
-  
-  #radius to focus on
-  scopeRadius <- as.numeric(input$radius)
-  
-  #################################################################################
-  #get affected stops within radius
-  
-  allStoppingPoints$distanceFromStation <- NA
-  colDistance <- which(colnames(allStoppingPoints)=="distanceFromStation")
-  
-  for (i in 1:nrow(allStoppingPoints)){
-    longLat <- c(allStoppingPoints[i,stop_long_col], allStoppingPoints[i,stop_lat_col])
-    allStoppingPoints[i,colDistance] <-  distGeo(stationLatLong, longLat)
-  }
-  
-  proximityStations <- subset(allStoppingPoints, distanceFromStation < scopeRadius)
-  
-  #################################################################################
-  #perform clustering
-  cbind(commuterData$destination_lat, commuterData$destination_long)
-  
-  kmeansOutput <- kmeans(cbind(commuterData$destination_lat, commuterData$destination_long), centers = 10)
-  
-  clusterCentres <- as.data.frame(kmeansOutput$centers)
-  colnames(clusterCentres) <- c("lat", "long")
-  clusterCentres$commutersDestinationInside <- NA
-  
-  for (clusterCentreRow in 1:nrow(clusterCentres)){
-    
-    clusterCentre <- c(clusterCentres[clusterCentreRow,2], clusterCentres[clusterCentreRow,1])
-    
-    tempDestinations <- cbind.data.frame(commuterData$destination_lat, commuterData$destination_long, NA)
-    colnames(tempDestinations) <- c("lat", "long", "distanceFromClusterCentre")
-    
-    for (i in 1:nrow(tempDestinations)){
-      longLat <- c(tempDestinations[i,2], tempDestinations[i,1])
-      tempDestinations[i,3] <- distGeo(clusterCentre, longLat)
-    }
-    
-    destinationsInCluster <- nrow(subset(tempDestinations, distanceFromClusterCentre < 2000))
-    
-    clusterCentres[clusterCentreRow,3] <- destinationsInCluster
-  }
-  
-  clusterCentres <- subset(clusterCentres, commutersDestinationInside > 30)
-    ################################################################################
-    #leaflet aestetics 
-    
-    mrtIcon <- icons(iconUrl = 'mrt_logo.png',   iconWidth = 25, iconHeight = 30)
+  output$label <- renderText({
     
     #################################################################################
-    #generate leaflet
-    leaflet() %>%
-      addLayersControl(
-        overlayGroups = c("Commuters' Destination", "Suggested Clusters", "All Bus Stops"),
-        options = layersControlOptions(collapsed = FALSE)
-      )%>%
-      addProviderTiles("CartoDB.Positron") %>%
-      addMarkers(lng=stationLatLong[1], lat=stationLatLong[2], icon = mrtIcon, popup=scopeRadius, group = "Affected Station") %>%
-      hideGroup("All Bus Stops") %>% 
-      addLegend('bottomright', title = "Commuters' Data", colors =c("blue", "red"), labels =c("Current", "Destinations")) %>%
-      # addCircles(lng =proximityStationslongtitude, lat=proximityStationslatitude, group = "Nearby Stations") %>%
-      addCircles(lng =commuterData$current_long, lat=commuterData$current_lat, group = "Commuters' Location") %>%
-      addCircles(lng =commuterData$destination_long, lat=commuterData$destination_lat, col = "red", group = "Commuters' Destination") %>%
-      addCircles(lng =allStoppingPoints$longtitude, lat=allStoppingPoints$latitude, col = "black", group = "All Bus Stops", radius = 10) %>%
-      addCircles(lng =clusterCentres$long, lat=clusterCentres$lat, col = "green", opacity = 0.2, radius = 2000, stroke=FALSE, group = "Suggested Clusters") 
-  })
-  
-  output$plotlyBarChart <- renderPlotly({
+    #dynamic parameters
     
-    ################################################################################
-    #input parameters
-    
-    #station name
-    stationName <- "Bishan MRT"
-    
-    #kent ridge coordinates
-    stationLatLong <- c(103.7827029, 1.2924922)
-    stationLatLong <-c(103.8469711, 1.3513141)
+    #default values in case shiny input fails   
+    scopeRadius <- 800
+    numberOfClusters <- 4
     
     #radius to focus on
     scopeRadius <- as.numeric(input$radius)
-    
+    numberOfClusters <- as.numeric(input$clusters)
     #################################################################################
     #get affected stops within radius
     
     #radius for bus stops
     allStoppingPoints$distanceFromStation <- NA
     colDistance <- which(colnames(allStoppingPoints)=="distanceFromStation")
+    
     for (i in 1:nrow(allStoppingPoints)){
       longLat <- c(allStoppingPoints[i,stop_long_col], allStoppingPoints[i,stop_lat_col])
       allStoppingPoints[i,colDistance] <-  distGeo(stationLatLong, longLat)
     }
+    
     proximityStops <- subset(allStoppingPoints, distanceFromStation < scopeRadius)
     
     #radius for commuters
@@ -174,8 +102,9 @@ shinyServer(function(input, output) {
       }
       
       
-      commuterData[i,closestBusStopCol] <- as.character(tempProximityBusStops[order(tempProximityBusStops$distanceToCommuter),][1,2])
+      commuterData[i,closestBusStopCol] <- as.character(tempProximityBusStops[order(tempProximityBusStops$distanceToCommuter),][1,4])
     }
+    
     #################################################################################
     #kmeans
     kmeansOutput <- kmeans(cbind(commuterData$destination_lat, commuterData$destination_long), centers = 10)
@@ -213,9 +142,9 @@ shinyServer(function(input, output) {
     }
     
     ###############
-    
-    #keep on clusters with more than 30 commuters
+    #keep on clusters with more than 30 commuters and subset based on shiny input
     clusterCentres <- subset(clusterCentres, commutersDestinationInside > 30)
+    clusterCentres <- head(clusterCentres, numberOfClusters)
     
     #keep only commuters in proximity of a populated cluster
     commutersInProximityOfClusterCentre <- subset(commutersInProximityOfClusterCentre, cluster_id %in% unique(clusterCentres$id))
@@ -245,6 +174,7 @@ shinyServer(function(input, output) {
     #################################################################################
     #get breakdown of each destination by origin
     odTable <- as.data.frame(table(commutersInProximityOfClusterCentre$closestStation, commutersInProximityOfClusterCentre$closestBusStop))
+    
     #################################################################################
     #prepare data for polty plot
     
@@ -252,6 +182,46 @@ shinyServer(function(input, output) {
     names(odTableWide) <- gsub("Freq.", "", names(odTableWide), fixed = TRUE)
     odTableWide$sum <- rowSums(odTableWide[, -1])
     odTableWide$Var1 <- factor(odTableWide$Var1, levels = unique(odTableWide$Var1)[order(odTableWide$sum, decreasing = TRUE)])
+    
+    ##########################################################################################
+    #output csv for other sections to use
+    write.csv(odTableWide, "odTableWide.csv", row.names = F)
+    write.csv(clusterCentres, "clusterCentres.csv", row.names = F)
+    
+    "data loaded"
+  })
+  
+  output$groundMap <- renderLeaflet({
+    
+    scopeRadius <- input$radius
+    clusterCount <- input$clusters
+    clusterCentres <- read.csv("clusterCentres.csv", stringsAsFactors = F)
+    ################################################################################
+    #leaflet aestetics 
+    mrtIcon <- icons(iconUrl = 'mrt_logo.png',   iconWidth = 25, iconHeight = 30)
+    #################################################################################
+    #generate leaflet
+    leaflet() %>%
+      addLayersControl(
+        overlayGroups = c("Commuters' Destination", "Suggested Clusters", "All Bus Stops"),
+        options = layersControlOptions(collapsed = FALSE)
+      )%>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addMarkers(lng=stationLatLong[1], lat=stationLatLong[2], icon = mrtIcon, popup=scopeRadius, group = "Affected Station") %>%
+      hideGroup("All Bus Stops") %>% 
+      addLegend('bottomright', title = "Commuters' Data", colors =c("blue", "red"), labels =c("Current", "Destinations")) %>%
+      # addCircles(lng =proximityStopslongtitude, lat=proximityStopslatitude, group = "Nearby Stations") %>%
+      addCircles(lng =commuterData$current_long, lat=commuterData$current_lat, group = "Commuters' Location") %>%
+      addCircles(lng =commuterData$destination_long, lat=commuterData$destination_lat, col = "red", group = "Commuters' Destination") %>%
+      addCircles(lng =allStoppingPoints$longtitude, lat=allStoppingPoints$latitude, col = "black", group = "All Bus Stops", radius = 10) %>%
+      addCircles(lng =clusterCentres$long, lat=clusterCentres$lat, col = "green", opacity = 0.2, radius = 2000, stroke=FALSE, group = "Suggested Clusters") 
+  })
+  
+  output$plotlyBarChart <- renderPlotly({
+    
+    scopeRadius <- input$radius
+    clusterCount <- input$clusters
+    odTableWide <- read.csv("odTableWide.csv", stringsAsFactors = F)
     
     #################################################################################
     #generate plotly
@@ -266,137 +236,13 @@ shinyServer(function(input, output) {
     plotlyBarChart
   })
   
+
   output$plotlySurfaceChart <- renderPlotly({
     
-    ################################################################################
-    #input parameters
+    scopeRadius <- input$radius
+    clusterCount <- input$clusters
+    odTableWide <- read.csv("odTableWide.csv", stringsAsFactors = F)
     
-    #station name
-    stationName <- "Bishan MRT"
-    
-    #kent ridge coordinates
-    stationLatLong <- c(103.7827029, 1.2924922)
-    stationLatLong <-c(103.8469711, 1.3513141)
-    
-    #radius to focus on
-    scopeRadius <- as.numeric(input$radius)
-    
-    #################################################################################
-    #get affected stops within radius
-    
-    #radius for bus stops
-    allStoppingPoints$distanceFromStation <- NA
-    colDistance <- which(colnames(allStoppingPoints)=="distanceFromStation")
-    for (i in 1:nrow(allStoppingPoints)){
-      longLat <- c(allStoppingPoints[i,stop_long_col], allStoppingPoints[i,stop_lat_col])
-      allStoppingPoints[i,colDistance] <-  distGeo(stationLatLong, longLat)
-    }
-    proximityStops <- subset(allStoppingPoints, distanceFromStation < scopeRadius)
-    
-    #radius for commuters
-    commuterData$distanceFromStation <- NA
-    colDistance <- which(colnames(commuterData)=="distanceFromStation")
-    for (i in 1:nrow(commuterData)){
-      longLat <- c(commuterData[i,current_long_col], commuterData[i,current_lat_col])
-      commuterData[i,colDistance] <-  distGeo(stationLatLong, longLat)
-    }
-    commuterData <- subset(commuterData, distanceFromStation < scopeRadius)
-    
-    #################################################################################
-    #assign commuter to nearest bus stop 
-    commuterData$closestBusStop <- NA
-    closestBusStopCol <- which(colnames(commuterData)=="closestBusStop")
-    for (i in 1:nrow(commuterData)){
-      longLat <- c(commuterData[i,current_long_col], commuterData[i,current_lat_col])
-      
-      tempProximityBusStops <- proximityStops
-      tempProximityBusStops$distanceToCommuter <- NA
-      distanceToCommuterCol <- which(colnames(tempProximityBusStops) == "distanceToCommuter")
-      
-      for (tempProximityBusStopsRow in 1:nrow(tempProximityBusStops)){
-        stopLongLat <- c(tempProximityBusStops[tempProximityBusStopsRow,stop_long_col], tempProximityBusStops[tempProximityBusStopsRow,stop_lat_col])
-        tempProximityBusStops[tempProximityBusStopsRow,distanceToCommuterCol] <-  distGeo(stopLongLat, longLat)
-      }
-      
-      
-      commuterData[i,closestBusStopCol] <- as.character(tempProximityBusStops[order(tempProximityBusStops$distanceToCommuter),][1,2])
-    }
-    #################################################################################
-    #kmeans
-    kmeansOutput <- kmeans(cbind(commuterData$destination_lat, commuterData$destination_long), centers = 10)
-    
-    ##########################
-    #get number of commuters within clusters and get the df of commuters within a cluster
-    
-    clusterCentres <- as.data.frame(kmeansOutput$centers)
-    colnames(clusterCentres) <- c("lat", "long")
-    
-    clusterCentres$id <- 1:nrow(clusterCentres)
-    clusterCentres$commutersDestinationInside <- NA
-    
-    commutersInProximityOfClusterCentre <- data.frame()
-    
-    for (clusterCentreRow in 1:nrow(clusterCentres)){
-      clusterCentre <- c(clusterCentres[clusterCentreRow,2], clusterCentres[clusterCentreRow,1])
-      clusterId <- clusterCentres[clusterCentreRow,3]
-      tempDestinations <- cbind.data.frame(commuterData$destination_lat, commuterData$destination_long, NA, commuterData$closestBusStop)
-      colnames(tempDestinations) <- c("lat", "long", "distanceFromClusterCentre", "closestBusStop")
-      
-      for (i in 1:nrow(tempDestinations)){
-        longLat <- c(tempDestinations[i,2], tempDestinations[i,1])
-        tempDestinations[i,3] <- distGeo(clusterCentre, longLat)
-      }
-      #rbind to compile a dataframe of commuters within a cluster
-      commutersWithinSpecificCluster <- subset(tempDestinations, distanceFromClusterCentre < 2000)
-      if(nrow(commutersWithinSpecificCluster) > 0){
-        commutersWithinSpecificCluster$cluster_id <- clusterId
-      }
-      commutersInProximityOfClusterCentre <- rbind(commutersInProximityOfClusterCentre,commutersWithinSpecificCluster)
-      
-      destinationsInCluster <- nrow(commutersWithinSpecificCluster)
-      clusterCentres[clusterCentreRow,4] <- destinationsInCluster
-    }
-    
-    ###############
-    
-    #keep on clusters with more than 30 commuters
-    clusterCentres <- subset(clusterCentres, commutersDestinationInside > 30)
-    
-    #keep only commuters in proximity of a populated cluster
-    commutersInProximityOfClusterCentre <- subset(commutersInProximityOfClusterCentre, cluster_id %in% unique(clusterCentres$id))
-    
-    ##########################
-    #get closest bus stop for cluster aka cluster name
-    
-    clusterCentres$closestStation <- NA
-    closestStationCol <- which(colnames(clusterCentres) == "closestStation")
-    
-    for (clusterCentreRow in 1:nrow(clusterCentres)){
-      clusterCentre <- c(clusterCentres[clusterCentreRow,2], clusterCentres[clusterCentreRow,1])
-      tempAllStoppingPoints <- allStoppingPoints[grep("STN", allStoppingPoints$name), ]
-      tempAllStoppingPoints <- cbind.data.frame(tempAllStoppingPoints$name, tempAllStoppingPoints$latitude, tempAllStoppingPoints$longtitude, NA)
-      colnames(tempAllStoppingPoints) <- c("name", "lat", "long", "distanceFromClusterCentre")
-      for (i in 1:nrow(tempAllStoppingPoints)){
-        longLat <- c(tempAllStoppingPoints[i,3], tempAllStoppingPoints[i,2])
-        tempAllStoppingPoints[i,4] <- distGeo(clusterCentre, longLat)
-      }
-      clusterCentres[clusterCentreRow, closestStationCol] <- as.character(tempAllStoppingPoints[order(tempAllStoppingPoints$distanceFromClusterCentre),][1,1])
-    }
-    
-    #get station name of commutersInProximityOfClusterCentre by refering to cluster_id
-    commutersInProximityOfClusterCentre <- merge(commutersInProximityOfClusterCentre, clusterCentres[ , c("id", "closestStation")],
-                                                 by.x=c("cluster_id"), by.y=c("id"))
-    
-    #################################################################################
-    #get breakdown of each destination by origin
-    odTable <- as.data.frame(table(commutersInProximityOfClusterCentre$closestStation, commutersInProximityOfClusterCentre$closestBusStop))
-    #################################################################################
-    #prepare data for polty plot
-    
-    odTableWide <- reshape(odTable, idvar = "Var1", timevar = "Var2", direction = "wide")
-    names(odTableWide) <- gsub("Freq.", "", names(odTableWide), fixed = TRUE)
-    odTableWide$sum <- rowSums(odTableWide[, -1])
-    odTableWide$Var1 <- factor(odTableWide$Var1, levels = unique(odTableWide$Var1)[order(odTableWide$sum, decreasing = TRUE)])
     #################################################################################
     #surface plot
     matrix3d <- as.matrix(odTableWide[,-c(1,ncol(odTableWide))])
@@ -421,5 +267,5 @@ shinyServer(function(input, output) {
     
     plotlySurfacePlot
   })
-
-})
+  
+  })
--- src/ui.R
+++ src/ui.R
@@ -17,7 +17,17 @@ shinyUI(pageWithSidebar(
                                    "1000m" = 1000,
                                    "1500m" = 1500,
                                    "2000m" = 2000),
-                                 selected = 800)
+                                 selected = 800),
+                     selectInput("clusters", "Clusters to Identify:",
+                                 c("1" = 1,
+                                   "2" = 2,
+                                   "3" = 3,
+                                   "4" = 4,
+                                   "5" = 5,
+                                   "6" = 6,
+                                   "7" = 7,
+                                   "8" = 8),
+                                 selected = 4)
     ),
     conditionalPanel(condition="input.conditionedPanels==2",
                      #yikun and yixin - add sidebar panel codes here
@@ -31,6 +41,7 @@ shinyUI(pageWithSidebar(
                  leafletOutput("groundMap")
                ), 
                fluidRow("Matching Commuters to Destinations", align="center"), 
+               fluidRow(textOutput("label")), 
                fluidRow(plotlyOutput(outputId = "plotlyBarChart")
                ),
                fluidRow(plotlyOutput(outputId = "plotlySurfaceChart")
